ca84adbb704f9304ef26009bdf70646cfbb7b0ea
uzaifahmedkhan <uzaifahmedkhan@gmail.com>
2024-06-18 13:29:34 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+//question no 1
+  console.log("hello world");
+
+//question no 2
+let myname = "uzaif"
+let message = "would you like to learn some typescript today"
+console.log(myname+" "+message);
